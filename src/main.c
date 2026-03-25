b3d5d58d4677751ddbee99434bf21a85cab48777
artem churakov <archourakov9@gmail.com>
2026-03-25 13:18:19 +0300

    added energy calibration matrix for different channels

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-//-- Includes ----------------------------------------------------------------
 #include <K1921VG015.h>
 #include <system_k1921vg015.h>
 #include "mtimer.h"
@@ -10,45 +9,24 @@
 #include "esp_comm.h"
 #include <string.h>
 
-//-- Defines ------------------------------------------------------------------
 #define LEDS_MSK  0xF000
 #define LED0_PIN  12
 #define LED0_MSK  (1 << LED0_PIN)
 
-#define TIME_EPS	  1.0E-5f
-#define MIN_DELTA     1.0E-3f
-#define DZ 		      1.0E-6f
+#define TIME_EPS	  1.0E-5
+#define MIN_DELTA     1.0E-3
+#define DZ 		      1.0E-6
 
+static double ENK0[6] = {-239.137, -118.336, -62.554, -35.514, -24.453, -14.380};
+static double ENK1[6] = {24.549, 12.396, 6.198, 3.122, 1.559, 0.780};
+static int8_t k_idx = 0;
 
-
-//-- Peripheral init functions ------------------------------------------------
 void led_init() 
 {
 	RCU->CGCFGAHB_bit.GPIOAEN = 1;
 	RCU->RSTDISAHB_bit.GPIOAEN = 1;
   	GPIOA->OUTENSET = LEDS_MSK;
 	GPIOA->DATAOUTCLR = LEDS_MSK;
-}
-
-void TMR32_IRQHandler();
-
-void TMR32_init(uint32_t period)
-{
-  RCU->CGCFGAPB_bit.TMR32EN = 1;
-  RCU->RSTDISAPB_bit.TMR32EN = 1;
-
-  //Записываем значение периода в CAPCOM[0]
-  TMR32->CAPCOM[0].VAL = period-1;
-  //Выбираем режим счета от 0 до значения CAPCOM[0]
-  TMR32->CTRL_bit.MODE = 1;
-
-  //Разрешаем прерывание по совпадению значения счетчика и CAPCOM[0]
-  TMR32->IM = 2;
-
-  // Настраиваем обработчик прерывания для TMR32
-  PLIC_SetIrqHandler (Plic_Mach_Target, IsrVect_IRQ_TMR32, TMR32_IRQHandler);
-  PLIC_SetPriority   (IsrVect_IRQ_TMR32, 0x1);
-  PLIC_IntEnable     (Plic_Mach_Target, IsrVect_IRQ_TMR32);
 }
 
 void periph_init() 
@@ -62,7 +40,6 @@ void periph_init()
 	OLED_init();
 }
 
-//--- USER FUNCTIONS -----------------------------------------------------------
 void check() 
 {
 	mtimer_sleep(800);
@@ -71,31 +48,27 @@ void check()
 	GPIOA->DATAOUTTGL = LED0_MSK;
 }
 
-float DoseRateInstant(uint32_t spectr[], uint16_t nchannels, float ltime, float Dz);
-
-//-- Main ----------------------------------------------------------------------
-volatile uint16_t nch = 128;
-uint16_t nch_max = 4096;
+float DoseRateInstant(uint32_t spectr[], uint16_t nchannels, float ltime, double Dz, uint8_t idx);
 
 int main(void) 
 {
     periph_init();
     check();
     
-    float ader = 0.0f, 
-          ltime = 0.0f, 
-          inprate = 0.0f;
+    static double ader = 0.0;
 
-    bool success = false,
+    static float ltime = 0.0, 
+                 inprate = 0.0;
+
+    static bool success = false,
 		 first_measure = false;
 		 
-    uint16_t nchan = 0;
     static uint32_t spectr[4096] = {};
     static uint32_t prev_spectr[4096] = {};
-    static float prev_ltime = 0.0f;
+    static double prev_ltime = 0.0;
     static char aderlen = 0;
-    static float en_k0 = -54.699f;
-    static float en_k1 = 0.808f;
+
+    int16_t nchan = 0;
 
     OLED_clear();
     OLED_setpos(48, 1);
@@ -105,20 +78,6 @@ int main(void)
     OLED_setpos(36, 6);
     OLED_printS("[uSv/h]", false);
 
-    InterruptDisable();
-
-    while(!success) { success = MODBUS_WriteSingleReg(SBS_ADDR, SBS_STATE_REG, 1); }
-    mtimer_sleep(1);
-    while(!success) { success = MODBUS_WriteSingleReg(SBS_ADDR, SBS_STATE_REG, 0); }
-    mtimer_sleep(1);
-    while(!success) { success = MODBUS_WriteSingleReg(SBS_ADDR, SBS_NCHANNELS_REG, 128);} 
-    mtimer_sleep(1);
-    while(!success) { success = MODBUS_WriteSingleReg(SBS_ADDR, SBS_STATE_REG, 2); }
-
-    TMR32_init((SystemCoreClock>>4) * 200);
-    
-    InterruptEnable();
-    
     while(1) {
         InterruptDisable();
         mtimer_sleep(1);
@@ -135,9 +94,34 @@ int main(void)
         success = MODBUS_ReadFloat(SBS_ADDR, SBS_INPRATE_REG, &inprate);
         if (!success) { ESP_Send_Error(); first_measure = true; continue; }
 
-        if (first_measure) {
+        switch(nchan) {
+            case 128: 
+                k_idx = 0;
+                break;
+            case 256: 
+                k_idx = 1;
+                break;
+            case 512: 
+                k_idx = 2;
+                break;
+            case 1024: 
+                k_idx = 3;
+                break;
+            case 2048: 
+                k_idx = 4;
+                break;
+            case 4096: 
+                k_idx = 5;
+                break;
+            default:
+                k_idx = -1;
+        }
+
+        if(k_idx == -1) continue;
+
+        if(first_measure) {
             // Первое измерение – мгновенная доза
-            ader = DoseRateInstant(spectr, nchan, ltime, DZ);
+            ader = DoseRateInstant(spectr, nchan, ltime, DZ, k_idx);
             ESP_SendFormatted("s,f,f,f", nchan, ader, ltime, inprate);
             memcpy(prev_spectr, spectr, nchan * sizeof(uint32_t));
             prev_ltime = ltime;
@@ -167,48 +151,39 @@ int main(void)
             }
 
             // Дифференциальный расчёт
-            float spectr_diff = 0.0f;
+            double enk0 = ENK0[k_idx];
+            double enk1 = ENK1[k_idx];
+            double sum_diff = 0.0;
             for (uint16_t i = 0; i < nchan; i++) {
                 uint32_t diff = spectr[i] - prev_spectr[i];
-                float E_kev = en_k0 + en_k1 * i;
-                spectr_diff += (float)diff * E_kev;
+                double E_kev = enk0 + enk1 * i;
+                sum_diff += (double)diff * E_kev;
             }
-            ader = (spectr_diff * DZ) / (delta_ltime);
-            ESP_SendFormatted("s,f,f,f", nchan, ader, ltime, inprate);
+            ader = (sum_diff * DZ) / delta_ltime;
+            float ader_f = (float)ader;
+
+            ESP_SendFormatted("s,f,f,f", nchan, ader_f, ltime, inprate);
             memcpy(prev_spectr, spectr, nchan * sizeof(uint32_t));
             prev_ltime = ltime;
 
-            aderlen = float_num_len(ader, 2);
+            aderlen = float_num_len(ader_f, 2);
             OLED_setpos((128 - aderlen * 8) / 2, 4);
-            OLED_printF(ader, 2, false);
+            OLED_printF(ader_f, 2, false);
         }
         InterruptEnable();
     }
     return 0;
 }
 
-float DoseRateInstant(uint32_t spectr[], uint16_t nchannels, float ltime, float Dz)
+float DoseRateInstant(uint32_t spectr[], uint16_t nchannels, float ltime, double Dz, uint8_t idx)
 {
-    float en_k0 = -54.699f;
-    float en_k1 = 0.808f;
-    float summ = 0.0f;
-    for(uint16_t i = 0; i < nchannels; i++) {
-        float E_kev = en_k0 + en_k1 * i;
-        summ += (float)spectr[i] * E_kev;
+    double enk0 = ENK0[idx];
+    double enk1 = ENK1[idx];
+    double summ = 0.0;
+    for (uint16_t i = 0; i < nchannels; i++) {
+        double E_kev = enk0 + enk1 * i;
+        summ += (double)spectr[i] * E_kev;
     }
-    float dose_rate = (summ * Dz) / (ltime);
-    return dose_rate;
-}
-
-void TMR32_IRQHandler()
-{
-    InterruptDisable();
-    bool success = MODBUS_WriteSingleReg(SBS_ADDR, SBS_STATE_REG, 1);
-    success = MODBUS_WriteSingleReg(SBS_ADDR, SBS_STATE_REG, 0);
-    nch = nch << 1;
-    if(nch > nch_max) nch = 128;
-    success = MODBUS_WriteSingleReg(SBS_ADDR, SBS_NCHANNELS_REG, nch);
-    success = MODBUS_WriteSingleReg(SBS_ADDR, SBS_STATE_REG, 2);
-    TMR32->IC = 3;
-    InterruptEnable();
+    double dose_rate = (summ * Dz) / ltime;
+    return (float)dose_rate;
 }
